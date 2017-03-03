8e4571e36aea3f8abdffb9be5c6b3ee44a484f30
Roman Bezak <r.bezak77@gmail.com>
2017-03-04 01:30:10 +0300

    Create class TaskManager class

--- src/client/platform/Task.java
+++ src/client/platform/Task.java
@@ -1,7 +1,5 @@
 package client.platform;
 
-/**
- * Created by admin on 04.03.2017.
- */
+
 public class Task {
 }
---
+++ src/client/platform/TaskManager.java
@@ -0,0 +1,7 @@
+package client.platform;
+
+/**
+ * Created by admin on 04.03.2017.
+ */
+public class TaskManager {
+}
