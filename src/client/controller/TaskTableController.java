bd4970178b4529c31703aa773003cdc2a6a487f3
Roman Bezak <r.bezak77@gmail.com>
2017-04-30 00:20:32 +0300

    Add icons to buttons

--- src/client/controller/TaskTableController.java
+++ src/client/controller/TaskTableController.java
@@ -1,5 +1,6 @@
 package client.controller;
 
+import client.platform.TaskManager;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -7,72 +8,121 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-/**
- * Created by admin on 04.03.2017.
- */
+
 public class TaskTableController {
 
 
-    private TableView table;
+    public TableView table;
+    public TaskManager task_manager;
 
-    private final ObservableList<Person> data =
-        FXCollections.observableArrayList(
-                new Person("Jacob", "Smith", "jacob.smith@example.com"),
-                new Person("Isabella", "Johnson", "isabella.johnson@example.com"),
-                new Person("Ethan", "Williams", "ethan.williams@example.com"),
-                new Person("Emma", "Jones", "emma.jones@example.com"),
-                new Person("Michael", "Brown", "michael.brown@example.com")
-        );
 
-    public TaskTableController(TableView table_link){
+
+    private final ObservableList<TasksInfoModel> tasks_list = FXCollections.observableArrayList();
+
+    public TaskTableController(TableView table_link, TaskManager task_manager_link){
 
         table = table_link;
+        task_manager = task_manager_link;
 
-        TableColumn emailCol = new TableColumn("Email");
-        emailCol.setMinWidth(200);
-        emailCol.setCellValueFactory(
-                new PropertyValueFactory<Person, String>("email"));
 
-        table.getColumns().addAll(emailCol);
+        this.setUpTableView();
 
-        table.setItems(data);
+        TableColumn id_col = new TableColumn("ID");
+        id_col.setMaxWidth(36);
+        id_col.setMinWidth(36);
+        id_col.setSortable(false);
+        id_col.setCellValueFactory(new PropertyValueFactory<TasksInfoModel, String>("id"));
+
+        TableColumn name_col = new TableColumn("Project Name");
+        name_col.setMaxWidth(250);
+        name_col.setMinWidth(250);
+        name_col.setSortable(false);
+        name_col.setCellValueFactory(new PropertyValueFactory<TasksInfoModel, String>("taskName"));
+
+        TableColumn url_col = new TableColumn("URL");
+        url_col.setMaxWidth(228);
+        url_col.setMinWidth(228);
+        url_col.setSortable(false);
+        url_col.setCellValueFactory(new PropertyValueFactory<TasksInfoModel, String>("urlSite"));
+
+        TableColumn status_col = new TableColumn("Status");
+        status_col.setMaxWidth(118);
+        status_col.setMinWidth(118);
+        status_col.setSortable(false);
+        status_col.setStyle("-fx-alignment: CENTER;");
+        status_col.setCellValueFactory(new PropertyValueFactory<TasksInfoModel, String>("status"));
+
+        table.getColumns().add(id_col);
+        table.getColumns().add(name_col);
+        table.getColumns().add(url_col);
+        table.getColumns().add(status_col);
+
+        table.setItems(tasks_list);
+
+        table.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            if (newSelection != null) {
+                System.out.println(table.getSelectionModel().getSelectedIndex());
+            }
+        });
 
     }
 
-    public static class Person {
+    public void setUpTableView(){
 
-        private final SimpleStringProperty firstName;
-        private final SimpleStringProperty lastName;
-        private final SimpleStringProperty email;
-
-        private Person(String fName, String lName, String email) {
-            this.firstName = new SimpleStringProperty(fName);
-            this.lastName = new SimpleStringProperty(lName);
-            this.email = new SimpleStringProperty(email);
+        for(int i = 0; i < task_manager.pool.tasks.size(); i++){
+            tasks_list.add(new TasksInfoModel(String.valueOf(i + 1), task_manager.pool.tasks.get(i).get("name").toString(),
+                                              task_manager.pool.tasks.get(i).get("url_site").toString(), "Stopped"));
         }
 
-        public String getFirstName() {
-            return firstName.get();
+    }
+
+    public static class TasksInfoModel {
+
+        private final SimpleStringProperty id;
+        private final SimpleStringProperty taskName;
+        private final SimpleStringProperty urlSite;
+        private final SimpleStringProperty status;
+
+        private TasksInfoModel(String _id, String _taskName, String _urlSite, String _status) {
+
+            this.id = new SimpleStringProperty(_id);
+            this.taskName = new SimpleStringProperty(_taskName);
+            this.urlSite = new SimpleStringProperty(_urlSite);
+            this.status = new SimpleStringProperty(_status);
+
         }
 
-        public void setFirstName(String fName) {
-            firstName.set(fName);
+
+        public void setId(String _id) {
+            id.set(_id);
         }
 
-        public String getLastName() {
-            return lastName.get();
+        public String getId() {
+            return id.get();
         }
 
-        public void setLastName(String fName) {
-            lastName.set(fName);
+        public void setTaskName(String _taskName) {
+            taskName.set(_taskName);
         }
 
-        public String getEmail() {
-            return email.get();
+        public String getTaskName() {
+            return taskName.get();
         }
 
-        public void setEmail(String fName) {
-            email.set(fName);
+        public void setUrlSite(String _urlSite) {
+            urlSite.set(_urlSite);
+        }
+
+        public String getUrlSite() {
+            return urlSite.get();
+        }
+
+        public void setStatus(String _status) {
+            status.set(_status);
+        }
+
+        public String getStatus() {
+            return status.get();
         }
     }
 
